e9287904828bada49b785fe92d79cdd1a71a75ba
ivan <inunez@alu.ucam.edu>
2019-07-31 20:08:13 +0200

    ejercicios

--- ejercicios1/6.c
+++ ejercicios1/6.c
@@ -5,7 +5,10 @@ int main() {
 #pragma omp parallel
   {
 #pragma omp master
-    { printf("master  %d\n", omp_get_thread_num()); }
+    {
+      printf("master  %d\n", omp_get_thread_num());
+    }
+#pragma omp barrier
     printf("%d\n", omp_get_thread_num());
   }
 
--- ejercicios2/1.c
+++ ejercicios2/1.c
@@ -1,12 +1,30 @@
 #include <omp.h>
 #include <stdio.h>
 
+void tarea_uno() { printf("tarea_uno\n"); }
+void tarea_dos() { printf("tarea_dos\n"); }
+
 int main() {
-#pragma omp parallel
+  double timeIni, timeFin;
+
+#pragma omp parallel sections
   {
-#pragma omp master
-    { printf("master  %d\n", omp_get_thread_num()); }
-    printf("%d\n", omp_get_thread_num());
+#pragma omp section
+    {
+      timeIni = omp_get_wtime();
+      printf("Ejecutando tarea 1\n");
+      tarea_uno();
+      timeFin = omp_get_wtime();
+      printf("Tiempo total = %f segundos\n", timeFin - timeIni);
+    }
+#pragma omp section
+    {
+      timeIni = omp_get_wtime();
+      printf("Ejecutando tarea 2\n");
+      tarea_dos();
+      timeFin = omp_get_wtime();
+      printf("Tiempo total = %f segundos\n", timeFin - timeIni);
+    }
   }
 
   return 0;
